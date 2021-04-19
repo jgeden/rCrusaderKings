6a112b1f0658de1144e460f506c17ee65cb810b5
Josh0823 <31145777+Josh0823@users.noreply.github.com>
2021-04-19 15:25:13 -0400

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# Reddit credentials
+client_id = ''
+client_secret = ''
+user_agent = ''
+
+# Twitter credentials
+consumer_key = ''
+consumer_key_secret = ''
+access_token = ''
+access_token_secret = ''
